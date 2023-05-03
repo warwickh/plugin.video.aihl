64f2bc0d5381079edbcf6a3b4f276296260fb8d3
Warwick Harris <warwick.harris@yahoo.com.au>
2023-05-03 17:24:27 +1000

    First working version

--- utils/aihlsession.py
+++ utils/aihlsession.py
@@ -16,7 +16,7 @@ import yaml
 class AihlSession:
     def __init__(self,
                  maxSessionTimeSeconds = 60 * 30,
-                 config = "config.yml",
+                 config = "/home/warwickh/.kodi/addons/plugin.video.aihl/config.yml",
                  **kwargs):
 
         self.config_filename = config
